6c75a13efa39d805285268dee7ec52853c62514f
JavierReyes1 <c00312829@setu.ie>
2025-11-20 23:22:59 +0000

    fixed few bugs and errors, added java server protocol and maso

--- init.lua
+++ init.lua
@@ -2,11 +2,19 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.signcolumn = "yes"
 vim.o.winborder = "rounded"
+vim.o.cursorcolumn = false
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
 vim.o.swapfile = false
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.ignorecase = true
 vim.g.mapleader = " "
+local map = vim.keymap.set
 vim.keymap.set('n', '<leader>w', ':w<CR>')
 vim.keymap.set('n', '<leader>o', ':update <CR> :source<CR>')
 
@@ -15,9 +23,10 @@ vim.keymap.set('n', '<leader>o', ':update <CR> :source<CR>')
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 --AutoCompletion
@@ -31,18 +40,24 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 vim.cmd("set completeopt+=noselect")
 
---mini.pick & oil setups
+--mini.pick, oil, mason setups
+require "mason".setup()
 require "mini.pick".setup()
 require "oil".setup()
+--treesitter
+require "nvim-treesitter.configs".setup({
+  ensure_installed = {"svelte", "typescript", "javascript", "java"},
+  highlight = {enable = true}
+})
 
 vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
 vim.keymap.set('n', '<leader>e', ':Oil<CR>')
-vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR')
-vim.keymap.set({'n', 'v', 'x'}, '<leader>d', '"+d<CR')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
 
---Enable Lsp
-vim.lsp.enable({ "lua_ls", "svelte-language-server", "tinymist" })
+ 
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.cmd("colorscheme vague")
